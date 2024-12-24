430daf8365583fbdd22296f08318e3c20c7f994b
jrdimande <timoteojrdimande@gmail.com>
2024-12-24 15:53:52 +0000

    Word and line counter

--- WordLineCounter.py
+++ WordLineCounter.py
@@ -9,22 +9,22 @@ search = input('Search a word: ').lower()
 num_words = 0
 num_search = 0
 
+aux = []
+
 for line in lines:
     current_line = line.split()
-    aux = []
-
 
     for word in current_line:
         aux.append(word.lower())
 
     for word in aux:
         if word == search:
-            num_search +=
-            
+            num_search += 1
+
 print(f'Number of words: {len(aux)}')
 
 if num_search > 0:
-    print(f'The word {search} appears {num_search} times')
+    print(f'The word {search} appears {int(num_search / 2)} times')
 else:
     print('word not founded')
 
